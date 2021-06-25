88ddfbd321f7550d8538c01d6dce71633360a6f6
NacryHugo <hugo.cymoz@gmail.com>
2021-06-25 14:23:05 +0200

    Hupeudateu

---
+++ assets/ffriends.js
@@ -0,0 +1,8 @@
+require('bootstrap');
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/ffriends.scss';
+import './styles/boostrap-custom.scss';
+
+// start the Stimulus application
+import './bootstrap';
